1757a32d7e328a6e2dea59bd391fe754a9cbb66a
pavithpr89 <pavithpr89@gmail.com>
2025-12-12 12:29:53 +0000

    Did the 14

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,24 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node
- * Return: Height of the tree, 0 if tree is NULL
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node
+ * Return: height of the tree, 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 size_t left_height, right_height;
 
-if (!tree)
+if (tree == NULL)
 return (0);
 
-if (!tree->left && !tree->right)
-return (0);
+left_height = (tree->left) ? 1 + binary_tree_height(tree->left) : 0;
+right_height = (tree->right) ? 1 + binary_tree_height(tree->right) : 0;
 
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
-
-if (left_height > right_height)
-return (left_height + 1);
-return (right_height + 1);
+return (left_height > right_height ? left_height : right_height);
 }
