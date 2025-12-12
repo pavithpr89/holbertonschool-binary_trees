6f12721445635334ebc45678dabf2db6d87289fa
pavithpr89 <pavithpr89@gmail.com>
2025-12-12 11:19:59 +0000

    Did the 1

--- binary_trees.h
+++ binary_trees.h
@@ -1,16 +1,7 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
-
+#include <stdio.h>
 #include <stdlib.h>
 
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
+/* Basic Binary Tree node */
 typedef struct binary_tree_s
 {
 	int n;
@@ -19,10 +10,11 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
-
+/* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
