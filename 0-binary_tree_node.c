b660ef59558077ceca2c0a8bf011b6a345086033
pavithpr89 <pavithpr89@gmail.com>
2025-12-12 11:11:59 +0000

    Did the 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,7 +4,6 @@
  * binary_tree_node - Creates a binary tree node
  * @parent: Pointer to the parent node
  * @value: Value to store in the new node
- *
  * Return: Pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
